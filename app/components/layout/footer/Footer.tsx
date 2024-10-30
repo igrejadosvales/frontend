444b7dcf116ce52d3b56b2ff12b6f267b6ff8d1b
Pedro Henrique <pedrohenriqueb366@gmail.com>
2024-10-29 21:51:58 -0300

    feat: add mobile section: music

--- app/components/layout/footer/Footer.tsx
+++ app/components/layout/footer/Footer.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Footer() {
   return ( 
-    <footer className="w-full bg-footer h-80 text-white">
-      <div className="max-w-screen-xl m-auto flex flex-col justify-between h-full pb-4 px-5">
-        <div className="flex flex-col gap-3">
+    <footer className="w-full bg-footer text-white">
+      <div className="max-w-screen-xl m-auto flex flex-col justify-between h-full pb-10 px-5">
+        <div className="flex flex-col gap-2 mb-10">
           <div>
             <a href="#">
               <Image src='icons/logoIVLS.svg' width={50} height={50} alt="logo footer"/>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,7 @@ const config: Config = {
       backgroundImage: {
         'background-header': "url('/global/background-header.webp')",
         'background-music': "url('/global/ivales_music.png')",
+        'background-music-mobile': "url('/global/ivales_music_mobile.png')",
       },
       borderRadius: {
         borderDrink: '2rem',
