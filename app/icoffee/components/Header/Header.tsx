e58f7c2934cbf384d1ce5a6f93bdfb589c553f3a
Pedro Henrique <pedrohenriqueb366@gmail.com>
2024-10-31 14:03:58 -0300

    feat: add logo icoffee

--- app/icoffee/components/Header/Header.tsx
+++ app/icoffee/components/Header/Header.tsx
@@ -1,8 +1,15 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 export default function Header() {
     return (
         <header className="pb-4 w-full">
-            <h1>logo</h1>
-            <h2>escolha seu
+            <div className='w-full flex justify-center pb-4'>
+                <Link href={'#'}>
+                    <Image src={'/icoffee/iCoffee.png'} alt='Logo iCoffee' width={230} height={230}></Image>
+                </Link>
+            </div>
+            <h2> escolha seu
             energético favorito!</h2>
 
             <nav className="w-full flex justify-between">
