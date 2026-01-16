94662a7d95d25d6d99aa3f43d7149abb522af75d
Isaque Böck <isaquebockdos@gmail.com>
2026-01-16 20:23:11 -0300

    Update sitemap URL path from `/pequenos-grupos` to `/igrupos`.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -29,7 +29,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
             priority: 0.8,
         },
         {
-            url: `${baseUrl}/pequenos-grupos`,
+            url: `${baseUrl}/igrupos`,
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.9,
